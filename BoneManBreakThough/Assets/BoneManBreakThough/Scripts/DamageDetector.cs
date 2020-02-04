b92870d6d8c446ef57e7d57ef2a9571cba79c13f
大胆包拯 <2295534490@qq.com>
2020-02-04 10:34:28 +0800

    角色死亡
    增加攻击信息attackInfo脚本,attackManage脚本
    修改attack脚本：增加攻击注册，取消攻击注册函数。
    增加DamageDector脚本，逐帧检测是否被攻击

---
+++ BoneManBreakThough/Assets/BoneManBreakThough/Scripts/DamageDetector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDetector : MonoBehaviour
+{
+    CharacterControl characterControl;
+
+    private void Awake()
+    {
+        characterControl = GetComponent<CharacterControl>();
+    }
+
+
+    private void Update()
+    {
+        if (AttackManager.Instance.CurrentAttacks.Count > 0)
+        {
+            CheckAttack();
+        }        
+    }
+
+    void CheckAttack()
+    {
+        foreach( AttackInfo info in AttackManager.Instance.CurrentAttacks)
+        {
+            if( info == null || !info.isRegisterd || info.Attacker==characterControl ||
+                info.isFinished || info.CurrentHits >= info.MaxHits)
+            {
+                continue;
+            }
+
+            if( info.MustCollide)
+            {
+                if( IsCollided(info))
+                {
+                    TakeDamage(info);
+                }
+            }
+
+        }
+    }
+
+    private bool IsCollided( AttackInfo info)
+    {
+        foreach( Collider collider in characterControl.CollidingParts)
+        {
+            foreach( string name in info.ColliderNames)
+            {
+                Debug.Log(name + "  " + collider.gameObject.name);
+                if (name == collider.gameObject.name)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void TakeDamage( AttackInfo info)
+    {
+        Debug.Log(info.Attacker.gameObject.name + " hits: " + this.gameObject.name);
+        characterControl.SkinnedMeshAnimator.runtimeAnimatorController = info.AttackAbility.GetDeathAnimator();
+        info.CurrentHits++;
+    }
+
+}
--- BoneManBreakThough/Assets/BoneManBreakThough/Scripts/States/Attack.cs
+++ BoneManBreakThough/Assets/BoneManBreakThough/Scripts/States/Attack.cs
@@ -5,19 +5,85 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New State", menuName = "Roundbeargames/AbilityData/Attack")]
 public class Attack : StateData
 {
+    public float StartAttackTime;
+    public float EndAttackTime;
+    public List<string> ColliderNames = new List<string>();
+    public bool MustCollide;
+    public bool MustFaceAttacker;
+    public float LethalRange;
+    public int MaxHits;
+    public List<RuntimeAnimatorController> DeathAnimator = new List<RuntimeAnimatorController>();
     public override void OnEnter(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
     {
         //Debug.Log("Attack Enter");
         animator.SetBool(TransitionParameter.Attack.ToString(), false);
+
+        GameObject obj = Instantiate(Resources.Load("AttackInfo", typeof(GameObject))) as GameObject;
+        AttackInfo info = obj.GetComponent<AttackInfo>();
+
+        info.ResetInfo(this);
+
+        if( !AttackManager.Instance.CurrentAttacks.Contains( info ))
+        {
+            AttackManager.Instance.CurrentAttacks.Add(info);
+        }
+    }
+
+    public void RegisterAttack(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
+    {        
+        if ( StartAttackTime<=stateInfo.normalizedTime&&EndAttackTime>stateInfo.normalizedTime)
+        {
+            Debug.Log( "   " + stateInfo.normalizedTime);
+            foreach ( AttackInfo info in AttackManager.Instance.CurrentAttacks)
+            {
+                if( info!=null && !info.isRegisterd&&info.AttackAbility==this)
+                {
+                    info.Register(this, characterStateBase.GetCharacterControl(animator));
+                }
+            }
+        }
+    }
+
+    public void DeregisterAttack(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
+    {
+        if( stateInfo.normalizedTime>=EndAttackTime)
+        {
+            foreach (AttackInfo info in AttackManager.Instance.CurrentAttacks)
+            {
+                if (info != null && info.AttackAbility == this && !info.isFinished )
+                {
+                    info.isFinished = true;
+                    Destroy(info.gameObject);
+                }
+            }
+        }
     }
 
     public override void OnExit(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
     {
-        
+        ClearAttack();
     }
 
     public override void UpdateAbility(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
     {
-        
+        RegisterAttack(characterStateBase, animator, stateInfo);
+        DeregisterAttack(characterStateBase, animator, stateInfo);
+    }
+
+    public void ClearAttack()
+    {
+        for( int i=0; i<AttackManager.Instance.CurrentAttacks.Count; ++i)
+        {
+            if( AttackManager.Instance.CurrentAttacks[i] == null || AttackManager.Instance.CurrentAttacks[i].isFinished)
+            {
+                AttackManager.Instance.CurrentAttacks.RemoveAt(i);
+            }
+        }
+    }
+
+    public RuntimeAnimatorController GetDeathAnimator()
+    {
+        int index = Random.Range(0, DeathAnimator.Count);
+        return DeathAnimator[index];
     }
 }
